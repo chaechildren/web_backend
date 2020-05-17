10cf54bdcdaaf04711ae5e2e3afddc9ce6bb387a
brokentime906 <btime906@gmail.com>
2020-05-17 17:07:41 +0900

    add v2 router 정리 완료 \#IOAJ-58

--- src/app.js
+++ src/app.js
@@ -24,7 +24,6 @@ app.use(morgan("dev")); //cli로 로그남김
 app.use(bodyParser.urlencoded({ extended: true })); // req.body 사용목적
 app.use(bodyParser.json());
 //router 설정
-<<<<<<< HEAD
 const homeRouter = require("./routes/v1/home");
 const arduinoRouter = require("./routes/v1/arduino");
 const phoneRouter = require("./routes/v1/phone");
@@ -35,14 +34,6 @@ app.use("/phone", phoneRouter);
 //router v2 설정
 const v2Router = require("./routes/v2");
 app.use("/v2", v2Router);
-=======
-const homeRouter = require("./routes/home");
-const arduinoRouter = require("./routes/arduino");
-const phoneRouter = require("./routes/phone");
-app.use("/", homeRouter);
-app.use("/arduino", arduinoRouter);
-app.use("/phone", phoneRouter);
->>>>>>> origin/master
 
 //Server Start
 app.listen(app.get("port"), () =>
