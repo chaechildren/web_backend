1eda4254243b23e45a5bc92b5cd1c26d4bebd6bf
brokentime906 <btime906@gmail.com>
2020-06-10 18:44:07 +0900

    add postgre 통합중
    \#IOAJ-83

--- src/routes/v2/phone.js
+++ src/routes/v2/phone.js
@@ -9,24 +9,6 @@ function splitTime(timeStr) {
   const _time = timeStr.substr(16, 8);
   return { _date, _time };
 }
-
-//개별 아두이노 데이터에 접근해서 return
-router.get("/data/:id", async (req, res, next) => {
-  const arduino_id = req.params.id;
-  const targetArd = await Arduino.findOne({ ID: arduino_id });
-  console.log(targetArd._id);
-
-  const datas = await Data.find({ arduino_id: targetArd._id });
-  // console.log(datas);
-  const processed_data = datas.map((data) => ({
-    date: splitTime(data.createdAt.toString())._date,
-    time: data.createdAt,
-    origin: data.createdAt,
-    humidity: data.humidity,
-  }));
-  res.json(processed_data);
-});
-
 router.post("/connect", async (req, res, next) => {
   const { arduino_id, user_id } = req.body;
   try {
@@ -45,6 +27,34 @@ router.post("/connect", async (req, res, next) => {
       msg: "연동 에러 발생 확인 필요.. POST: phone/connect",
     });
   }
+});
+//개별 아두이노 데이터에 접근해서 return
+router.get("/data/:id", async (req, res, next) => {
+  const arduino_id = req.params.id;
+  const targetArd = await Arduino.findOne({ ID: arduino_id });
+  console.log(targetArd._id);
+
+  const datas = await Data.find({ arduino_id: targetArd._id });
+  // console.log(datas);
+  const processed_data = datas.map((data) => ({
+    date: splitTime(data.createdAt.toString())._date,
+    time: data.createdAt,
+    origin: data.createdAt,
+    humidity: data.humidity,
+  }));
+  res.json(processed_data);
+});
+
+
+router.post("/setTemp:id",async (req, res, next) => {
+  const arduino_id = req.params.id;
+  const {temp}= req.body;
+  const targetArd = await Arduino.findOne({ ID: arduino_id });
+  console.log(targetArd._id);
+  const newData = await Arduino.update({targetArd._id},{  })
+  // const 
+  // res.json(processed_data);
+  res.json(hi:"Hi");
 });
 
 module.exports = router;
